395fe49b9a17bf58140e51b6b1ca21da5bac8279
Mad Neutrino <madneutrino@jezer0x.com>
2025-01-21 09:58:01 +0800

    Show attachment in modal. A bit janky, should expand to fill the size of the content, but works in principle.

--- src/pages/patient/PatientSearchPage.tsx
+++ src/pages/patient/PatientSearchPage.tsx
@@ -1,11 +1,27 @@
-import { Paper } from '@mantine/core';
-import { DEFAULT_SEARCH_COUNT, formatSearchQuery, parseSearchRequest, SearchRequest } from '@medplum/core';
-import { Patient } from '@medplum/fhirtypes';
-import { Loading, MemoizedSearchControl, useMedplum } from '@medplum/react';
+import { Modal, Paper, Title, Text } from '@mantine/core';
+import {
+  DEFAULT_SEARCH_COUNT,
+  formatSearchQuery,
+  getDisplayString,
+  getReferenceString,
+  parseSearchRequest,
+  SearchRequest,
+} from '@medplum/core';
+import { Patient, Resource } from '@medplum/fhirtypes';
+import {
+  AttachmentDisplay,
+  Document,
+  Loading,
+  ResourcePropertyDisplay,
+  ResourceTable,
+  SearchControl,
+  useMedplum,
+} from '@medplum/react';
 import { useEffect, useState } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import { usePatient } from '../../hooks/usePatient';
 import { prependPatientPath } from './PatientPage.utils';
+import { useDisclosure } from '@mantine/hooks';
 
 export function PatientSearchPage(): JSX.Element {
   const medplum = useMedplum();
@@ -13,6 +29,8 @@ export function PatientSearchPage(): JSX.Element {
   const navigate = useNavigate();
   const location = useLocation();
   const [search, setSearch] = useState<SearchRequest>();
+  const [opened, { open, close }] = useDisclosure(false);
+  const [selectedResource, setSelectedResource] = useState<Resource>();
 
   useEffect(() => {
     if (!patient) {
@@ -37,20 +55,31 @@ export function PatientSearchPage(): JSX.Element {
   }
 
   return (
-    <Paper shadow="xs" m="md" p="xs">
-      <MemoizedSearchControl
-        checkboxesEnabled={true}
-        search={search}
-        onClick={(e) => navigate(`/Patient/${patient.id}/${e.resource.resourceType}/${e.resource.id}`)}
-        onAuxClick={(e) => window.open(`/Patient/${patient.id}/${e.resource.resourceType}/${e.resource.id}`, '_blank')}
-        onNew={() => {
-          navigate(prependPatientPath(patient, `/${search.resourceType}/new`));
-        }}
-        onChange={(e) => {
-          navigate(`/Patient/${patient.id}/${search.resourceType}${formatSearchQuery(e.definition)}`);
-        }}
-      />
-    </Paper>
+    <>
+      <Paper shadow="xs" m="md" p="xs">
+        <SearchControl
+          checkboxesEnabled={true}
+          search={search}
+          onClick={(e) => {
+            setSelectedResource(e.resource);
+            open();
+            // navigate(`/Patient/${patient.id}/${e.resource.resourceType}/${e.resource.id}`)
+          }}
+          onAuxClick={(e) =>
+            window.open(`/Patient/${patient.id}/${e.resource.resourceType}/${e.resource.id}`, '_blank')
+          }
+          onNew={() => {
+            navigate(prependPatientPath(patient, `/${search.resourceType}/new`));
+          }}
+          onChange={(e) => {
+            navigate(`/Patient/${patient.id}/${search.resourceType}${formatSearchQuery(e.definition)}`);
+          }}
+        />
+      </Paper>
+      <Modal opened={opened} onClose={close}>
+        {selectedResource && <ResourceViewer resource={selectedResource} />}
+      </Modal>
+    </>
   );
 }
 
@@ -70,4 +99,35 @@ function addSearchValues(patient: Patient, search: SearchRequest): SearchRequest
     offset,
     count,
   };
+}
+
+interface ResourceViewerProps {
+  resource: Resource;
+}
+
+function ResourceViewer({ resource }: ResourceViewerProps): JSX.Element {
+  if (resource.resourceType === 'DocumentReference') {
+    const category = resource.category?.[0]?.coding?.[0]?.display || 'Uncategorized';
+    const type = resource.type?.coding?.[0]?.display || 'Unknown Type';
+
+    return (
+      <Document key={getReferenceString(resource)}>
+        <Title>{`${category} - ${type}`}</Title>
+        {resource.content?.map((content, index) => (
+          <div key={index}>
+            <ResourcePropertyDisplay value={resource.date} propertyType={'dateTime'} />
+            {content.attachment && <AttachmentDisplay value={content.attachment} />}
+            <Text>{resource.description}</Text>
+          </div>
+        ))}
+      </Document>
+    );
+  }
+
+  return (
+    <Document key={getReferenceString(resource)}>
+      <Title>{getDisplayString(resource)}</Title>
+      <ResourceTable key={`${resource.resourceType}/${resource.id}`} value={resource} />
+    </Document>
+  );
 }
